e2b9b9986a5a18ddf4311305a2287fe56fdcb03c
DEVils <techanalogy.mait@gmail.com>
2024-02-13 14:37:40 +0530

    done

---
+++ src/views/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Spline from '@splinetool/react-spline';
+
+function Index() {
+    return (
+        <div className='w-screen h-screen flex items-center justify-center'>
+            <Spline scene="https://prod.spline.design/WYNe8QHf-WB56UIe/scene.splinecode" />
+        </div>
+    )
+}
+
+export default Index
