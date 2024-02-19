28a22fd22fff06c82dc8c55293ff1b64eea52f59
Aman <123443485+exploring-solver@users.noreply.github.com>
2024-02-19 12:31:43 +0530

    Update NavbarTop.jsx

--- src/components/NavbarTop.jsx
+++ src/components/NavbarTop.jsx
@@ -4,22 +4,24 @@ import ModelViewerTwo from './ModelViewerTwo';
 
 const NavbarTop = () => {
   return (
-    <nav className="bg-black pb-5 relative">
+    <nav className="bg-black py-6 sticky top-0">
       <div className="max-w-7xl mx-auto px-4">
-        <div className="flex justify-between items-center">
-          <div className="flex-shrink-0 text-white">
+        <div className=" items-center">
+          <div className=" text-white">
             {/* <img src="byte-logo-transparent.png" className='w-20 p-4' alt="" /> */}
             {/* <div className='fixed top-20 -right-20 z-30'>
               <ModelViewerTwo />
             </div> */}
-            <div className='fixed -top-7 -left-20 z-30'>
+            <div className='fixed -top-6 -left-16 z-30'>
               <ModelViewerTwo />
             </div>
           </div>
-          <div className="hidden sm:block">
-            <div className="flex space-x-4">
+          <div className=" flex items-start pl-32">
+            <div className="flex  items-center justify-between">
+
               <NavLink exact to="/home" activeClassName="bg-gray-700" className="text-white hover:bg-gray-700 px-4 py-4 text-sm font-medium">Home</NavLink>
               <NavLink to="/about" activeClassName="bg-gray-700" className="text-white hover:bg-gray-700 px-4 py-4 text-sm font-medium">About</NavLink>
+
               <NavLink to="/achievements" activeClassName="bg-gray-700" className="text-white hover:bg-gray-700 px-4 py-4 text-sm font-medium">Achievements</NavLink>
               <NavLink to="/tasks" activeClassName="bg-gray-700" className="text-white hover:bg-gray-700 px-4 py-4 text-sm font-medium">Challenges</NavLink>
               <NavLink to="/team" activeClassName="bg-gray-700" className="text-white hover:bg-gray-700 px-4 py-4 text-sm font-medium">Team</NavLink>
