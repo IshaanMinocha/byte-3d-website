1a45ed0a18b6dce932c5880bd6d263ffd698019d
exploring-solver <amansharma12607@gmail.com>
2024-02-19 20:07:56 +0530

    hmmmmmm

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -121,7 +121,7 @@ function Hero() {
               Selection Challenges
             </button>
           </Link>
-          <Link to='/join'>
+          <Link to='https://github.com/bytemait'>
             <button className="px-6 py-2 font-semibold bg-purple text-light w-fit transition-all shadow-[3px_3px_0px_black] active:shadow-none active:translate-x-[3px] hover:text-dark active:translate-y-[3px] ">
               Our Projects
             </button>
